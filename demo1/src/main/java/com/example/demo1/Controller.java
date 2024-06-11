2d494324590267b580172bdd4a2412913140e647
tcp <tcp.dev.app@gmail.com>
2024-06-11 20:25:01 +0200

    volcado

---
+++ demo1/src/main/java/com/example/demo1/Controller.java
@@ -0,0 +1,18 @@
+package com.example.demo1;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebHistory;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+}
